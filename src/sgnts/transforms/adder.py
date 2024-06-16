287f48396cc6c5ad81b6da81d8a7878d90106301
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-06-16 06:05:40 -0700

    adder.py: subclass TSTransform for Adder, to use already synchronized frames

--- src/sgnts/transforms/adder.py
+++ src/sgnts/transforms/adder.py
@@ -7,6 +7,7 @@ from ..base import Audioadapter, SeriesBuffer, TSFrame, TSTransform
 import numpy as np
 
 
+
 @dataclass
 class Adder(TSTransform):
     """
@@ -14,40 +15,26 @@ class Adder(TSTransform):
     """
 
     def __post_init__(self):
-        self.inbufs = {}
         super().__post_init__()
-        self.sample_rate = None
-
-    def pull(self, pad, bufs):
-        self.inbufs[pad] = bufs
-        # This is a subclass of TSTransform, so the frames are already aligned
-        if self.sample_rate is None and len(bufs.buffers) > 0:
-            self.sample_rate = bufs[0].sample_rate
-        else:
-            for buf in bufs:
-                assert buf.sample_rate == self.sample_rate 
 
     def transform(self, pad):
-        EOS = any(b.EOS for b in self.inbufs.values())
+        frames = list(self.preparedframes.values())
+        assert len(set(f.sample_rate for f in frames)) == 1
+        assert len(set(f.offset for f in frames)) == 1
+        assert len(set(f.end_offset for f in frames)) == 1
 
-        print('inbufs in adder',self.inbufs)
-        frames = [b.buffers for b in self.inbufs.values()]
-        if len(frames[0]) == 0:
-            return TSFrame(EOS=EOS)
-        else:
-            # use the first frame as basis
-            out = np.concatenate([buf.filleddata for buf in frames[0]])
-            # add to the first frame
-            for f in frames[1:]:
-                i0 = 0
-                for buf in f:
-                    if not buf.is_gap:
-                        out[...,i0:i0+buf.samples] += buf.data
-                    i0 += buf.samples
+        # use the first frame as basis
+        out = np.concatenate([buf.filleddata for buf in frames[0]])
+        # add to the first frame
+        for f in frames[1:]:
+            i0 = 0
+            for buf in f:
+                if not buf.is_gap:
+                    out[...,i0:i0+buf.samples] += buf.data
+                i0 += buf.samples
 
-            return TSFrame(buffers=[SeriesBuffer(
-                offset=frames[0][0].offset,
-                sample_rate=self.sample_rate,
-                data=out,
-            )], EOS=EOS)
-
+        return TSFrame(buffers=[SeriesBuffer(
+            offset=frames[0].offset,
+            sample_rate=frames[0].sample_rate,
+            data=out,
+        )], EOS=frames[0].EOS)
