5eba1c70f493f9f732961c00af2737011f5c6b02
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-06-22 12:07:01 -0700

    update resampler to use TSTransSink base class

--- src/sgnts/transforms/resampler.py
+++ src/sgnts/transforms/resampler.py
@@ -30,7 +30,7 @@ class Resampler(TSTransform):
         factor = self.outrate / self.inrate
         self.factor = factor
         self.next_out_offset = None
-        self.audioadapter = Audioadapter()
+        # self.audioadapter = Audioadapter()
 
         if self.outrate < self.inrate:
             # downsample parameters
@@ -43,48 +43,16 @@ class Resampler(TSTransform):
             self.kernel_length = self.half_length * 2 + 1
             self.thiskernel = self.upkernel(factor)
 
-        self.pad_length = self.half_length
+        self.history_pad_samples = self.half_length
+        self.future_pad_samples = self.half_length
 
         super().__post_init__()
+
+        self.pad_length = self.half_length
+
         assert (
             len(self.sink_pads) == 1 and len(self.source_pads) == 1
         ), "only one sink_pad and one source_pad is allowed"
-
-    def pull(self, pad, bufs):
-        """
-        assumes there is only one sink pad, if the user wants
-        to resample multitple channels of data,
-        connect multiple resampler elements
-        """
-        self.inbufs = bufs
-        for buf in bufs:
-            assert buf.sample_rate == self.inrate, (
-                f"data sample rate: {buf.sample_rate}"
-                f" does not match resampler sample rate: {self.inrate}"
-            )
-            self.audioadapter.push(buf)
-            if self.next_out_offset is None:
-                # start offset counter with the offset of the very first buffer
-                self.next_out_offset = buf.offset
-
-    def pad_func(self, data):
-        npad = [(0, 0)] * data.ndim
-        npad[-1] = (self.pad_length, 0)
-        return np.pad(data, npad, "constant")
-
-    def get_output_length(self):
-        """
-        Needs half_length of data on each side, will use all the samples available
-        in the audioadapter
-        """
-        # Pretend that we have a half_length set of samples if we are at a discont
-        pretend_samps = self.pad_length
-        numinsamps = self.audioadapter.size + pretend_samps
-        nout = int((numinsamps - self.kernel_length + 1) * self.factor)
-        if nout < 0:
-            nout = 0
-
-        return numinsamps, nout
 
     def downkernel(self, factor: float):
         """
@@ -120,7 +88,7 @@ class Resampler(TSTransform):
 
         return vecs.reshape(int(factor), 1, sub_kernel_length)
 
-    def resample(self, data0, output_length):
+    def resample(self, data0, outshape):
         data = data0.reshape(-1, data0.shape[-1])
 
         if self.factor > 1:
@@ -136,60 +104,20 @@ class Resampler(TSTransform):
             out = correlate(data, self.thiskernel, mode="valid")[
                 ..., :: int(1 / self.factor)
             ]
-
-        out = out.reshape(data0.shape[:-1] + (output_length,))
-
-        return out
+        return out.reshape(outshape)
 
     def transform(self, pad):
-        """
-        Up/down samples buffers
-        """
-        EOS = self.inbufs.EOS
-
-        A = self.audioadapter
-        assert self.next_out_offset == A.offset + Offset.fromsamples(
-            self.half_length - self.pad_length, self.inrate
-        ), (
-            f"offset mismatch: {self.next_out_offset=}, {A.offset=}, "
-            f"{self.half_length=}, {self.pad_length=}, "
-            f"{Offset.fromsamples(self.half_length-self.pad_length,self.inrate)=}"
-        )
-
-        sampsin, output_length = self.get_output_length()
-        output_shape = self.inbufs.shape[:-1] + (output_length,)
-
-        if output_length == 0:
+        frame = self.preparedframes[self.sink_pads[0]]
+        outshape = self.preparedoutframes[self.sink_pads[0]].shape
+        if frame.shape[-1] == 0:
             outdata = None
         else:
-            if A.is_gap() is True:
-                # Produce a single gap buffer if all the data in the
-                # audioadapter are gaps
+            if frame.is_gap:
+                # if all([b.is_gap for b in frame.buffers]):
                 outdata = None
             else:
-                # copy out everything in the audioadapter
-                # we will resample everything that is available
-                outdata, _, copied_nongap = A.copy_samples(A.size)
-                if self.pad_length > 0:
-                    # if we need to pad half length of zeros in front
-                    outdata = self.pad_func(outdata)
-                # resample the data
-                outdata = self.resample(outdata, output_length)
+                outdata = self.resample(frame.buffers[0].data, outshape)
 
-            # flush samples from audioadapter
-            # leave some leftover samples to pad infront of next buffer
-            flush_nsamples = A.size - self.half_length * 2
-            A.flush_samples(flush_nsamples)
-
-            # update pad_length for the next buffer
-            self.pad_length = -min(0, flush_nsamples)
-
-        outbuf = SeriesBuffer(
-            offset=self.next_out_offset,
-            sample_rate=self.outrate,
-            data=outdata,
-            shape=self.inbufs.shape[:-1] + (output_length,),
-        )
-        # shift the next output buffer's offset starting point
-        self.next_out_offset += Offset.fromsamples(output_length, self.outrate)
-        return TSFrame(buffers=[outbuf], EOS=EOS)
+        outframe = self.preparedoutframes[self.sink_pads[0]]
+        outframe.buffers[0].update_data(outdata)
+        return outframe
