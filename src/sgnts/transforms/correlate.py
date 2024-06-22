a32fe4a1129db9b15d0b3acc71c5e5c489b682ea
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-06-22 12:06:21 -0700

    update correlate to use the TSTransSink base class

--- src/sgnts/transforms/correlate.py
+++ src/sgnts/transforms/correlate.py
@@ -6,11 +6,11 @@ import numpy as np
 import scipy
 from sgn.base import TransformElement
 
-from ..base import Audioadapter, Offset, SeriesBuffer, TSFrame
+from ..base import Audioadapter, Offset, SeriesBuffer, TSFrame, TSTransform
 
 
 @dataclass
-class Correlate(TransformElement):
+class Correlate(TSTransform):
     """
     Correlates input data with filters
 
@@ -28,74 +28,26 @@ class Correlate(TransformElement):
 
     def __post_init__(self):
         assert self.filters is not None
-        self.audioadapter = Audioadapter()
         super().__post_init__()
         assert (
             len(self.sink_pads) == 1 and len(self.source_pads) == 1
         ), "only one sink_pad and one source_pad is allowed"
 
-    def pull(self, pad, bufs):
-        """
-        Assumes there is only one sink pad, if the user wants
-        to correltate multitple channels of data,
-        connect multiple correlate elements
-        """
-        self.inbufs = bufs
-        self.nnew = 0  # len of the new data
-        for buf in bufs:
-            self.audioadapter.push(buf)
-            self.nnew += buf.samples
-        offset0 = bufs[0].offset
-        offset1 = bufs[-1].offset + bufs[-1].noffset
-
-        # the offset segment we want to produce
-        self.this_segment = (offset0, offset1)
-        self.this_noffset = offset1 - offset0
-        self.sample_rate = bufs[0].sample_rate
-
     def transform(self, pad):
         """
         Correlates data with filters
         """
-        inbufs = self.inbufs
-        EOS = inbufs.EOS
+        data = self.preparedframes[self.sink_pads[0]].buffers[0].data
 
-        nfilter_samples = self.filters.shape[-1]
-
-        A = self.audioadapter
-
-        shift = Offset.fromsamples(nfilter_samples - 1, self.sample_rate)
-
-        start = max(0, self.this_segment[0] - shift)
-
-        request_segment = (start, self.this_segment[1])
-
-        data, _, _ = A.copy_samples_by_offset_segment(request_segment)
-        nworkspace = self.nnew + nfilter_samples - 1
-        ndata = data.shape[-1]
-
-        if ndata < nworkspace:
-            data = np.pad(data, (nworkspace - ndata, 0), "constant")
-
-        os = []
+        # FIXME: consider gaps
         # FIXME: Are there multi-channel correlation in numpy or scipy?
+        # FIXME: consider multi-dimensional filters
+        os = []
         for i in range(self.filters.shape[0]):
             os.append(scipy.signal.correlate(data, self.filters[i], mode="valid"))
         out = np.vstack(os)
 
-        next_offset = request_segment[1] - shift
-
-        if next_offset > A.get_available_offset_segment()[0]:
-            A.flush_samples_by_end_offset_segment(next_offset)
-
-        return TSFrame(
-            buffers=[
-                SeriesBuffer(
-                    offset=self.this_segment[0],
-                    sample_rate=inbufs[-1].sample_rate,
-                    data=out,
-                    shape=self.filters.shape[:-1] + (self.nnew,),
-                )
-            ],
-            EOS=EOS,
-        )
+        outframe = self.preparedoutframes[self.sink_pads[0]]
+        outshape = self.filters.shape[:-1] + (outframe.shape[-1],)
+        outframe.buffers[0].update_data(out, shape=outshape)
+        return outframe
