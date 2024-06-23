e6600423a5c63bbdbce78441b328244edb8853d4
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-06-23 10:25:35 -0700

    update gaps in adder

--- src/sgnts/transforms/adder.py
+++ src/sgnts/transforms/adder.py
@@ -21,16 +21,22 @@ class Adder(TSTransform):
         assert len(set(f.sample_rate for f in frames)) == 1
         assert len(set(f.offset for f in frames)) == 1
         assert len(set(f.end_offset for f in frames)) == 1
+        assert len(set(f.shape for f in frames)) == 1
 
-        # use the first frame as basis
-        out = np.concatenate([buf.filleddata for buf in frames[0]], axis=-1)
-        # add to the first frame
-        for f in frames[1:]:
-            i0 = 0
-            for buf in f:
-                if not buf.is_gap:
-                    out[..., i0 : i0 + buf.samples] += buf.data
-                i0 += buf.samples
+        if all(frame.is_gap for frame in frames):
+            out = None
+            shape = frames[0].shape
+        else:
+            # use the first frame as basis
+            out = np.concatenate([buf.filleddata for buf in frames[0]], axis=-1)
+            shape = out.shape
+            # add to the first frame
+            for f in frames[1:]:
+                i0 = 0
+                for buf in f:
+                    if not buf.is_gap:
+                        out[..., i0 : i0 + buf.samples] += buf.data
+                    i0 += buf.samples
 
         return TSFrame(
             buffers=[
@@ -38,6 +44,7 @@ class Adder(TSTransform):
                     offset=frames[0].offset,
                     sample_rate=frames[0].sample_rate,
                     data=out,
+                    shape=shape,
                 )
             ],
             EOS=frames[0].EOS,
