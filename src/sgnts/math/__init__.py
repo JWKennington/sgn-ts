3396505cc8b1aef783b421a8b7df1f1ddfaab75d
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-07-01 07:51:10 -0700

    numpy library

---
+++ src/sgnts/math/__init__.py
@@ -0,0 +1,13 @@
+import numpy as np
+def cat_func(xs, axis):
+    return np.concatenate(xs, axis=axis)
+
+def pad_func(data, pad_samples: tuple):
+    npad = [(0, 0)] * data.ndim
+    npad[-1] = pad_samples
+    return np.pad(data, npad, "constant")
+
+def full_func(shape, fill_value):
+    return np.full(shape, fill_value)
+
+zeros_func = np.zeros
