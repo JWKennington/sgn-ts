8646b0c7adf38e184c4f4bf1ccaaf6624c9fee3a
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-07-15 08:33:03 -0700

    add ArrayOps class

---
+++ src/sgnts/base/array_ops.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class ArrayOps:
+    def cat_func(xs, axis):
+        return np.concatenate(xs, axis=axis)
+
+    def pad_func(data, pad_samples: tuple):
+        npad = [(0, 0)] * data.ndim
+        npad[-1] = pad_samples
+        return np.pad(data, npad, "constant")
+
+    def full_func(shape, fill_value):
+        return np.full(shape, fill_value)
+
+    zeros_func = np.zeros
