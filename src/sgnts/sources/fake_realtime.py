641a24256eec709a0bec1e090f29dd604a5a2700
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-10-10 08:19:21 -0400

    RealTimeWhiteNoiseSrc: produce realtime buffers against clock

--- src/sgnts/sources/fake_realtime.py
+++ src/sgnts/sources/fake_realtime.py
@@ -1,5 +1,5 @@
+import time
 from dataclasses import dataclass
-from time import sleep
 
 import numpy as np
 
@@ -12,19 +12,33 @@ class RealTimeWhiteNoiseSrc(TSSource):
     A time-series source that generates fake data in fixed-size buffers in real-time
     """
 
-    rate: int = 2048
     num_buffers: int = 10
 
     def __post_init__(self):
         super().__post_init__()
         self.cnt = {p: 0 for p in self.source_pads}
         self.shape = (self.num_samples,)
-        self.duration = self.num_samples / self.rate
+        if self.rate != self.num_samples:
+            raise ValueError(
+                f"Can only produce 1 second buffers, rate={self.rate}"
+                f" num_samples={self.num_samples}"
+            )
+        self.next_time = None
 
     def new(self, pad):
         self.cnt[pad] += 1
 
-        sleep(self.duration)
+        # Produce buffers at every integer second time
+        now = time.time()
+        if self.next_time is None:
+            self.next_time = int(now) + 1
+
+        sleep = self.next_time - now
+        if sleep > 0:
+            # There might be cases where sleep < 0 and we are behind? In that case
+            # don't sleep
+            time.sleep(sleep)
+        self.next_time = int(time.time()) + 1
 
         data = np.random.randn(self.num_samples)
 
