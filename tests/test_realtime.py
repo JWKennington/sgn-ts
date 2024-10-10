686063b6edf53af5f32152155635363170a4a599
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-10-10 08:16:24 -0400

    update test_realtime.py

--- tests/test_realtime.py
+++ tests/test_realtime.py
@@ -3,7 +3,7 @@
 from sgn.apps import Pipeline
 
 from sgnts.sinks import FakeSeriesSink
-from sgnts.sources import FakeRealtimeSrc
+from sgnts.sources import RealTimeWhiteNoiseSrc
 
 
 def test_realtime(capsys):
@@ -15,7 +15,7 @@ def test_realtime(capsys):
     t0 = 0.0
     num_buffers = 2
     pipeline.insert(
-        FakeRealtimeSrc(
+        RealTimeWhiteNoiseSrc(
             name="src",
             source_pad_names=("H1",),
             rate=inrate,
