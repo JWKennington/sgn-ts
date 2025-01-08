05d52ccd66f5357be9ac1910ef02c37d943a8417
Hanna <crh184@e1-056827.science.psu.edu>
2025-01-08 09:04:41 -0500

    fix coverage typing and lint checks

--- tests/test_realtime.py
+++ tests/test_realtime.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
 
 from sgn.apps import Pipeline
-
+import time
 from sgnts.sinks import FakeSeriesSink
 from sgnts.sources import FakeSeriesSource
+from sgnts.base import TSTransform
 
 
 def test_realtime(capsys):
 
+    class Slow(TSTransform):
+        def __post_init__(self):
+            super().__post_init__()
+            self.slowcounter = 0
+
+        def new(self, pad):
+            if self.slowcounter == 1:
+                time.sleep(2)
+            self.slowcounter += 1
+            return self.preparedframes[self.sink_pads[0]]
+
     pipeline = Pipeline()
 
     inrate = 256
-    duration = 2
+    duration = 4
     pipeline.insert(
         FakeSeriesSource(
             name="src",
@@ -20,13 +32,15 @@ def test_realtime(capsys):
             duration=duration,
             real_time=True,
         ),
+        Slow(name="slow", source_pad_names=("H1",), sink_pad_names=("H1",)),
         FakeSeriesSink(
             name="snk",
             sink_pad_names=("H1",),
             verbose=True,
         ),
         link_map={
-            "snk:sink:H1": "src:src:H1",
+            "slow:sink:H1": "src:src:H1",
+            "snk:sink:H1": "slow:src:H1",
         },
     )
 
